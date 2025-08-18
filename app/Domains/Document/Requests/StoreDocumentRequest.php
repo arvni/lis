6146c9bbef8f1fd976068266d5225b4e464e28ed
Arvin Izadi <arvinizadi1991@gmail.com>
2025-08-18 12:08:13 +0400

    add the size of upload file

--- app/Domains/Document/Requests/StoreDocumentRequest.php
+++ app/Domains/Document/Requests/StoreDocumentRequest.php
@@ -14,7 +14,7 @@ class StoreDocumentRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'file' => ['required', 'file', 'max:10240'], // 10MB max size
+            'file' => ['required', 'file', 'max:20480'], // 20MB max size
             'tag' => ['nullable', 'string'],
             'ownerClass'=>[ 'nullable', 'string'],
             'ownerId'=>[ 'nullable', 'string'],
