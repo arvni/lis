b9b629c5b13ce0c28710402589e2ddad32378243
Arvin Izadi <arvinizadi1991@gmail.com>
2025-10-14 16:10:34 +0400

    bug

--- app/Domains/Reception/Repositories/AcceptanceRepository.php
+++ app/Domains/Reception/Repositories/AcceptanceRepository.php
@@ -357,10 +357,10 @@ class AcceptanceRepository
                             $method = Arr::first($referrerTestMethod);
                             $item["method_test"]["method"] = [
                                 ...$item["method_test"]["method"],
-                                "extra" => $method["extra"],
+                                "extra" => $method["extra"]??[],
                                 "price" => $method["price"],
                                 "price_type" => $method["price_type"],
-                                "referrer_extra" => $method["extra"],
+                                "referrer_extra" => $method["extra"]??[],
                                 "referrer_price" => $method["price"],
                                 "referrer_price_type" => $method["price_type"],
                             ];
